5283d346948588cf2e861e11af0d3ba30acbcfe5
ktraw <ktraw2@gmail.com>
2018-11-08 19:07:23 -0800

    Create the initial graphical window

---
+++ src/MainCanvas.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainCanvas extends Canvas {
+
+  public MainCanvas(JFrame gameWindow)
+  {
+    super();
+  }
+}
--- src/MainDesktop.java
+++ src/MainDesktop.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class MainDesktop {
-    public static void main(String[] args) {
-      System.out.println("Hello, World!");
-    }
+
+  final static Dimension WINDOW_DIMENSION = new Dimension(1280, 720);
+
+  public static void main (String[] args)
+  {
+    JFrame mainFrame = new JFrame();
+    MainCanvas mainCanvas = new MainCanvas(mainFrame);
+    mainCanvas.setSize(WINDOW_DIMENSION);
+    mainCanvas.setPreferredSize(WINDOW_DIMENSION);
+    mainFrame.add(mainCanvas);
+    mainFrame.getContentPane().setSize(WINDOW_DIMENSION);
+    mainFrame.getContentPane().setPreferredSize(WINDOW_DIMENSION);
+    mainFrame.pack();
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // mainFrame.setTitle(mainCanvas.getGameTitle());
+    mainFrame.setVisible(true);
+  }
+
 }
