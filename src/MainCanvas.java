ee4bfb039aaddb27dc03d5daf321a3bd3036f72d
ktraw <ktraw2@gmail.com>
2018-11-29 19:47:44 -0800

    Fleshed out wall collision detection

--- src/MainCanvas.java
+++ src/MainCanvas.java
@@ -77,10 +77,25 @@ public class MainCanvas extends Canvas implements KeyListener, Runnable {
               future = new Point(player.getX() + SPEED, player.getY());
               break;
           }
-          // Check collisions with walls
-          if (future != null && (future.getX() >= 0 && future.getX() + player.getWidth(null) < this.getWidth())
-              && (future.getY() >= 0 && future.getY() + player.getHeight(null) < this.getHeight())) {
-            player.setLocation(future);
+          // Ensure the player stays within the window
+          if (future != null) {
+            if (future.getX() < 0) {
+              future.setLocation(0, future.getY());
+            }
+            if (future.getY() < 0) {
+              future.setLocation(future.getX(), 0);
+            }
+            if (future.getX() >= 0 && future.getY() >= 0) {
+              if (future.getX() + player.getWidth(null) < this.getWidth() && future.getY() + player.getHeight(null) < this.getHeight()) {
+                player.setLocation(future);
+              } else if (future.getX() + player.getWidth(null) < this.getWidth()) {
+                player.setLocation(new Point((int) future.getX(), this.getHeight() - player.getHeight(null)));
+              } else if (future.getY() + player.getHeight(null) < this.getHeight()) {
+                player.setLocation(new Point(this.getWidth() - player.getWidth(null), (int) future.getY()));
+              } else {
+                player.setLocation(new Point(this.getWidth() - player.getWidth(null), this.getHeight() - player.getHeight(null)));
+              }
+            }
           }
         }
       }
